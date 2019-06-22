2f178dc8fa146b777a6ac42725dac585dbdd92ff
gunny <corqkrtk3@naver.com>
2019-06-22 21:17:44 +0900

    [TEST] Piece 종류별 테스트 케이스 작성

--- src/test/java/domain/pieces/PieceTest.java
+++ src/test/java/domain/pieces/PieceTest.java
@@ -9,10 +9,31 @@ public class PieceTest {
     public void create_piece() {
         verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
         verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
     }
 
     private void verifyPiece(Piece piece, final String color, final char representation) {
         assertThat(piece.getColor()).isEqualTo(color);
         assertThat(piece.getRepresentation()).isEqualTo(representation);
+    }
+
+    @Test
+    public void isWhiteAndBlack() {
+        assertThat(Piece.createWhiteKing().isWhite()).isTrue();
+        assertThat(Piece.createWhiteKing().isBlack()).isFalse();
+
+        assertThat(Piece.createBlackBishop().isBlack()).isTrue();
+        assertThat(Piece.createBlackBishop().isWhite()).isFalse();
     }
 }
